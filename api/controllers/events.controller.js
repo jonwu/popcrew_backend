b168f9c88e02ae62d29359fb18df79d4c37274e4
Jonathan Wu <jonwu92@gmail.com>
2018-03-26 01:02:40 -0700

    fix

--- api/controllers/events.controller.js
+++ api/controllers/events.controller.js
@@ -15,10 +15,10 @@ exports.list = function(req, res) {
 
 exports.create = function(req, res) {
   if (req.body.valid_days) req.body.valid_days = req.body.valid_days.split(',');
-  if (req.body.users) req.body.users.split(',');
-  if (req.body.groups) req.body.groups.split(',');
+  if (req.body.users) req.body.users = req.body.users.split(',');
+  if (req.body.groups) req.body.groups = req.body.groups.split(',');
   Object.keys(req.body).forEach((key) => !req.body[key] && delete req.body[key]);
-
+  console.log(req.body)
   var new_event = new Event(req.body);
   new_event
     .save()
@@ -34,8 +34,8 @@ exports.read = function(req, res) {
 
 exports.update = function(req, res) {
   if (req.body.valid_days) req.body.valid_days = req.body.valid_days.split(',');
-  if (req.body.users) req.body.users.split(',');
-  if (req.body.groups) req.body.groups.split(',');
+  if (req.body.users) req.body.users = req.body.users.split(',');
+  if (req.body.groups) req.body.groups = req.body.groups.split(',');
   Object.keys(req.body).forEach((key) => !req.body[key] && delete req.body[key]);
 
   Event.findOneAndUpdate({ _id: req.params.eventId }, req.body, { new: true })
